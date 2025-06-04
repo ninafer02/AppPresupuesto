fc866f395933b54417e794ecc711c68bb952ff09
fer <fer.sofia.avila.barraza@gmail.com>
2025-06-04 16:27:11 -0400

    guardar

--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
 let ingresosHTML="";
 let totalIngresos=0;
-
+let EgresoHTML="";
+let totalEgreso=0;
 let agregarDato =(event) =>{
   event.preventDefault();
   
@@ -13,11 +14,11 @@ let agregarDato =(event) =>{
     console.log(" valor "+ valor);
     if(tipo==="ingreso"){
       cargarIngresos(descripcion, Number(valor));
+      document.getElementById("descripcion").value="";
+      document.getElementById("valor").value="";
     }else if(tipo==="egreso"){
-      cargarEgreso(descripcion, Number(valor));
+      //hace otra cosa
     }
-    document.getElementById("descripcion").value="";
-    document.getElementById("valor").value="";
   }else{
     alert("debe completar todos los campos")
   }
@@ -25,7 +26,7 @@ let agregarDato =(event) =>{
 } 
 
 let cargarIngresos = (descripcion, valor) =>{
-  ingresosHTML +=crearIngresosHTML(descripcion, valor);
+  ingresosHTML+=crearIngresosHTML(descripcion, valor);
   totalIngresos+=valor;
   document.getElementById("ingresoTotal").textcontent = formatearCLP(totalIngresos);
   document.getElementById("presupuesto").textContent = formatearCLP(totalIngresos);
@@ -43,7 +44,7 @@ let crearIngresosHTML=(descripcion, valor)=>{
               <ion-icon name="close-circle-outline"></ion-icon>
           </button>
       </div>
-  </div>
+   </div>
 </div>`;
 }
 
@@ -53,4 +54,32 @@ function formatearCLP(numero) {
       currency: 'CLP',
       minimumFractionDigits: 0
   }).format(numero);
+}
+let cargarEgresoHTML=(descripcion, Valor)=>{
+EgresoHTML -=CrearEgresoHTML(descripcion, Valor);
+totalEgreso-=Valor;
+document.getElementById("Egresototal").textContent = formatearCLP(totalEgreso);
+document.getElementById("presupuesto").textcontent = formatearCLP(totalEgreso);
+document.getElementById("lista-Egreso").innerHTML=EgresoHTML;
+}
+
+let CrearEgresoHTML=(descripcion, Valor)=>{
+  return `<div class="elemento limpiarEstilos">
+  <div class="elemento_descripcion">${descripcion}</div>
+  <div class="derecha limpiarEstilos">
+     <div class="elemento_valor">${formatearCLP(valor)}</div>
+     <div class="elemento_eliminar">
+       <button class="elemento_eliminar--btn">
+       <ion-icon name="close-circule-outline"></ion-icon>
+       </button>
+     </div>
+   </div>
+ </div>`
+}
+function formatearCLP(numero) {
+  return new Intl.NumberFormat('es-CL', {
+    style: 'currency',
+    currency: 'CLP',
+    minimumFractionDigits: 0
+}).format(numero);
 }
